bb986099df50f725c4c023443863b2fce3510bb0
Brad Wang <bradwangw@163.com>
2021-01-20 08:57:44 +0000

    Calculated derivative

--- poly_derivative.py
+++ poly_derivative.py
@@ -7,8 +7,14 @@ a3 = 0.2
 # Point at which we evaluate the derivative
 x0 = 3.1
 
+coeff = [a0, a1, a2, a3]
+coeff_prime = []
+for i in range(1, 4):
+    coeff_prime.append(i * coeff[i])
 
-p_prime = # ...
+print(coeff_prime)
+
+p_prime = coeff_prime[0] + coeff_prime[1] * x0 + coeff_prime[2] * x0 ** 2
 
 
 
